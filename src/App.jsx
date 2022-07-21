99e60e1d53270e458009087189f6a45983edb243
rafaelsoeiro <103294767+rafaelsoeiro@users.noreply.github.com>
2022-07-21 07:02:13 -0300

    Crete index.css

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Services from './components/services/Services'
+import Experience from './components/experience/Experience'
+import Portfolio from './components/portfolio/Porrfolio'
+import Testmonials from './components/testmonials/Testmonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Services />
+            <Experience/>
+            <Portfolio />
+            <Testmonials />
+            <Contact />
+            <Footer />
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/testmonials/Testmonials.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Testmonials.css";
+
+const Testmonials = () => {
+    return <div>testmonials</div>;
+};
+
+export default Testmonials;
