2268103ec82f93c09580db33a031921ae75a03d9
rafaelsoeiro <103294767+rafaelsoeiro@users.noreply.github.com>
2022-07-26 22:49:22 -0300

    creacte components
    create footer end fix navbar

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 import { BsGithub } from "react-icons/bs";
-import { IoIosMail } from 'react-icons/io';
+import { IoIosMail } from "react-icons/io";
 import { ImLinkedin } from "react-icons/im";
 import { ImWhatsapp } from "react-icons/im";
 
 import "./Contact.css";
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "service_736i7np",
+                "template_h6cdkeo",
+                form.current,
+                "JKRs9KZ5FdLhPR-rK"
+            )
+            e.target.reset()
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+    };
     return (
         <section id="contact">
             <h5> Com apenas um toque</h5>
@@ -16,30 +40,65 @@ const Contact = () => {
             <div className="container contact-container">
                 <div className="contact-options">
                     <article className="contact-option">
-                        <ImLinkedin className="contact-option-icon"/>
+                        <ImLinkedin className="contact-option-icon" />
                         <h4>Linkedin</h4>
                         <h5>in/rafael-soeiro/</h5>
-                        <a className="btn" href="https://www.linkedin.com/in/rafael-soeiro/" target="_blank">Ver Perfil</a>
+                        <a
+                            className="btn"
+                            href="https://www.linkedin.com/in/rafael-soeiro/"
+                            target="_blank"
+                        >
+                            Ver Perfil
+                        </a>
                     </article>
                     <article className="contact-option">
-                        <IoIosMail className="contact-option-icon"/>
+                        <IoIosMail className="contact-option-icon" />
                         <h4>email</h4>
                         <h5>rafaelssoeiro@gmail.com</h5>
-                        <a className="btn" href="mailto:rafaelssoeiro@gmail.com">Entrar em contato</a>
+                        <a
+                            className="btn"
+                            href="mailto:rafaelssoeiro@gmail.com"
+                        >
+                            Entrar em contato
+                        </a>
                     </article>
                     <article className="contact-option">
-                        <ImWhatsapp className="contact-option-icon"/>
+                        <ImWhatsapp className="contact-option-icon" />
                         <h4>Whatsapp</h4>
                         <h5>+55 (98) 98488-3851</h5>
-                        <a className="btn" href="https://wa.me/5598984883851" target="_blank">Entrar em contato</a>
+                        <a
+                            className="btn"
+                            href="https://wa.me/5598984883851"
+                            target="_blank"
+                        >
+                            Entrar em contato
+                        </a>
                     </article>
                 </div>
                 {/* Fim das opções de contato */}
-                <form action="">
-                    <input type="text" name="name" placeholder="Seu nome completo" required/>
-                    <input type="email" name="email" placeholder="Seu email para contato" required/>
-                    <textarea name="msg" id="" rows="10" placeholder="Sua mensagem" required></textarea>
-                    <button className="btn btn-primary"type="submit">Enviar Mensagem</button>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Seu nome completo"
+                        required
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Seu email para contato"
+                        required
+                    />
+                    <textarea
+                        name="message"
+                        id=""
+                        rows="10"
+                        placeholder="Sua mensagem"
+                        required
+                    ></textarea>
+                    <button className="btn btn-primary" type="submit">
+                        Enviar Mensagem
+                    </button>
                 </form>
             </div>
         </section>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './Footer.css'
+
+import { BsGithub } from "react-icons/bs";
+import { IoIosMail } from "react-icons/io";
+import { ImLinkedin } from "react-icons/im";
+import { ImWhatsapp } from "react-icons/im";
+
 const Footer = () => {
   return (
     <footer id='footer'>
-      footer
+        <a className="footer-logo" href="">Rafael Soeiro</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">Sobre mim</a></li>
+        <li><a href="#experience">Teck Stack</a></li>
+        <li><a href="#service">serviços</a></li>
+        <li><a href="#portfolio">portfolio</a></li>
+        <li><a href="#testmonials">Depoimentos</a></li>
+        <li><a href="#contact">Contato</a></li>
+      </ul>
+
+      <div className="footer-socials">
+        <a href="https://www.linkedin.com/in/rafael-soeiro/"> <ImLinkedin/></a>
+        <a href="https://github.com/rafaelsoeiro"><BsGithub/></a>
+        <a href="https://wa.me/5598984883851"><ImWhatsapp/></a>
+      </div>
+      <div className="footer-copy">
+        <small>&copy; Rafael Soeiro</small>
+      </div>
     </footer>
   )
 }
